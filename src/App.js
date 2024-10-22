9baa403abd999e3ae13d7a208690e36c048e7d30
shunyaoyangshunyao <u7341225@anu.edu.au>
2024-10-22 21:18:10 +1100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [newsList, setNewsList] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.spaceflightnewsapi.net/v4/articles/")
+      .then((response) => response.json())
+      .then((data) => {
+        setNewsList(data.results);
+        console.log(data.results);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="title">
+        <h1> Space News </h1>
+      </div>
+      <div className="newsContainer">
+        {newsList.map((val, key) => {
+          return (
+            <div
+              key={key}
+              className="article"
+              onClick={() => {
+                window.location.href = val.url;
+              }}
+            >
+              <h3> {val.title} </h3>
+              <img src={val.image_url} />
+              <p> {val.summary} </p>
+              <h4> {val.published_at} </h4>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
